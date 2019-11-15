36db12fc0965704469039ec120a3590b351983f9
Melquisedeque Pereira <melqui1@live.com.pt>
2019-11-15 17:10:21 +0000

    Added authenticate middleware with jwt

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -4,5 +4,18 @@
 */
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+  const secret = (process.env.NODE_ENV === 'development' ? 'secret' : process.env.JWT_SECRET);
+  if (token) {
+    jwt.verify(token, secret, (err, decodedUser) => {
+      if (err) {
+        next({ message: err, status: 400 });
+      } else {
+        req.loggedInUser = decodedUser;
+        next();
+      }
+    });
+  } else {
+    next({ message: "YOU SHALL NOT PASS!", status: 401 });
+  }
 };
